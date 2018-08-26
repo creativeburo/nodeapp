44f067c81329301cc217f2f77b84348e87afa023
AlexF <office@creativeburo.com>
2018-08-26 12:20:54 +0300

    Add Bootstrap

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // prepare server
-app.use('/api', api); // redirect API calls
-app.use('/', express.static(__dirname + '/www')); // redirect root
+
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
