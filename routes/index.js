6ee015bf8f49c924c97c25370742499eb3734e7b
AlexF <office@creativeburo.com>
2018-08-26 13:33:23 +0300

    Add Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@ var router = express.Router();
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'dbuser',
-    password : 's3kreee7'
+    host     : '145.239.244.116',
+    user     : 'c2VchrmDBUsr',
+    password : 'bRewrad9UsWE',
+    database : 'c2MtcktDb'
 });
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+connection.query('SELECT * FROM td_Cat', function(err, rows, fields) {
     if (err) throw err;
     console.log('The solution is: ', rows[0].solution);
 });
